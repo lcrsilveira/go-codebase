03cbbaed53e675404d92365e1a2a2107740602d2
Luis Silveira <lcrsilveira@gmail.com>
2022-08-28 09:57:53 -0400

    variable and type declaration

---
+++ course/typedeclaration.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// var greetingText string
+	// greetingText = "Hello World!"
+
+	// var greetingText string = "Hello World!"
+
+	// := infers var and type declaration
+	greetingText := "Hello World!"
+
+	fmt.Println(greetingText)
+	fmt.Println(greetingText)
+}
